d0325ddbf8b7a3fbfbc2e8af5e21ce7753ad32b3
cesarenegro <cesare.negro@gmail.com>
2026-06-01 14:29:15 +0800

    Initial Commit

---
+++ Arkai MD/Arkai_MDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Arkai_MDApp.swift
+//  Arkai MD
+//
+//  Created by Cesare on 01/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Arkai_MDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
